f9b6d80582a139045525af4fe4a4e6bb8f32c5dd
rahulfordev <rahulforsms@gmail.com>
2023-12-18 21:42:19 +0600

    navbar design finished

--- src/components/RigthNav.jsx
+++ src/components/RigthNav.jsx
@@ -1,14 +1,38 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+
+import user from "../assets/user-photo.png";
+
 const RigthNav = () => {
   return (
     <div className="right-nav">
-      <div>
-        <h1>Rigth</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab similique,
-          pariatur aliquid libero sapiente possimus adipisci! Vero, itaque
-          placeat debitis exercitationem, explicabo provident iusto possimus
-          harum neque, nobis illo sit.
-        </p>
+      <div className="right__nav--container">
+        <div className="right__nav--users">
+          <img width="50px" src={user} alt="curr-users" />
+          <h2>Mark Zuckerberg</h2>
+        </div>
+        <div className="right__nav--icons">
+          <div className="right__nav--icon">
+            <div className="right__nav--profile">
+              <AccountCircleIcon />
+            </div>
+
+            <span>Profile</span>
+          </div>
+          <div className="right__nav--icon">
+            <div className="right__nav--profile">
+              <NotificationsIcon />
+            </div>
+            <span>Mute</span>
+          </div>
+          <div className="right__nav--icon">
+            <div className="right__nav--profile">
+              <SearchIcon />
+            </div>
+            <span>Search</span>
+          </div>
+        </div>
       </div>
     </div>
   );
